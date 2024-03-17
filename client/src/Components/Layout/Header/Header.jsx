fa428694b8903b56c5d89c012bbddbdca06c5072
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-03-17 14:50:25 +1100

    Added logo for webpage header

--- client/src/Components/Layout/Header/Header.jsx
+++ client/src/Components/Layout/Header/Header.jsx
@@ -1,7 +1,10 @@
+import logo from "../../../assets/Logo.svg";
+import "./Header.css";
 
 function Header() {
     return(
         <>
+            <img alt="logo" src={logo} className="logo"></img>
         </>
     );
 }
