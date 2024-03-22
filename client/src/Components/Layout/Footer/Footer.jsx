d2b64e2e3c709a71ad17f5cfb9f98f220770c28c
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-03-22 14:09:07 +1100

    added footer section

--- client/src/Components/Layout/Footer/Footer.jsx
+++ client/src/Components/Layout/Footer/Footer.jsx
@@ -1,8 +1,10 @@
+import "./Footer.css"
 
 function Footer() {
     return(
-        <>
-        </>
+        <div className="footer">
+            <h2 className="footer-text">Footer goes here</h2>
+        </div>
     );
 }
 
