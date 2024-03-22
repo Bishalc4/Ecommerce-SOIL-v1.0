1f85d4558d641d368eb05251fc4d3f5ed0537d0a
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-03-22 14:08:51 +1100

    import bootstrap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './Components/Layout/Header/Header.jsx'
 import Navbar from './Components/Layout/Navbar/Navbar.jsx'
 import Footer from './Components/Layout/Footer/Footer.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
