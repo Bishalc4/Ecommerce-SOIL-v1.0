984733ecf43d42bc4fccc7aa39a51641e2e0d4db
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-03-22 14:09:18 +1100

    implemented navbar

--- client/src/Components/Layout/Navbar/Navbar.jsx
+++ client/src/Components/Layout/Navbar/Navbar.jsx
@@ -1,8 +1,18 @@
+import "./Navbar.css"
+import { FiMenu } from "react-icons/fi";
 
 function Navbar() {
     return(
-        <>
-        </>
+        <div className="navbar">
+            <a href="#">
+                <FiMenu />
+                Browse
+            </a>
+            <a href="#">Specials</a>
+            <a href="#">Diet/Nutrition</a>
+            <a href="#">Grow It Yourself</a>
+            <a href="#">About</a>
+        </div>
     );
 }
 
