da8833d7048e37193325fc68bb7dc9193ed6b8b0
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-03-24 16:36:28 +1100

    Set up react routers for navbar

--- client/src/Components/Layout/Navbar/Navbar.jsx
+++ client/src/Components/Layout/Navbar/Navbar.jsx
@@ -1,26 +1,37 @@
-import "./Navbar.css"
+import { Link } from 'react-router-dom';
 import { FiMenu } from "react-icons/fi";
+import "./Navbar.css"
 
 function Navbar() {
     return(
         <nav className="navbar">
             <ul>
-                <li><a href="#">
-                    <FiMenu />
-                    <span>Browse</span>
-                </a></li>
-                <li><a href="#">
-                    <span>Specials</span>
-                </a></li>
-                <li><a href="#">
-                    <span>Diet/Nutrition</span>
-                </a></li>
-                <li><a href="#">
-                    <span>Grow It Yourself</span>
-                </a></li>
-                <li><a href="#">
-                    <span>About</span>
-                </a></li>
+                <li>
+                    <Link to="/">
+                        <FiMenu />
+                        <span>Browse</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/">
+                        <span>Specials</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/">
+                        <span>Diet/Nutrition</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/">
+                        <span>Grow It Yourself</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/">
+                        <span>About</span>
+                    </Link>
+                </li>
             </ul>
         </nav>
     );
