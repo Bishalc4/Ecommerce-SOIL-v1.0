24009a833e4e3ee08da58d5b68e82bf178220117
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-03-29 15:01:54 +1100

    Implemented sign up page

---
+++ client/src/Pages/SignUp/SignUp.jsx
@@ -0,0 +1,10 @@
+import AuthHeader from "../../Components/Layout/Auth-Header/AuthHeader";
+
+function SignUp() {
+    <div className="sign-up-container">
+        <AuthHeader />
+        <hr></hr>
+    </div>
+}
+
+export default SignUp
