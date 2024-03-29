b425fced89b26e190ff1aa71c2360c904635e796
Bishalc4 <s4009432@student.rmit.edu.au>
2024-03-29 15:10:13 +1100

    login-styling

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -7,6 +7,22 @@ function Login() {
         <div className="login-container">
             <AuthHeader />
             <hr></hr>
+            <form>
+                <div className="form-group">
+                    <label className="label-control">Username</label>
+                    <input className="input-control" />
+                </div>
+
+                <div className="form-group">
+                    <label className="label-control">Password</label>
+                    <input type="password" className="input-control" />
+                </div>
+
+                <div className="form-group">
+                    <input type="submit" className="btn btn-primary"/>
+                </div>
+
+            </form>
         </div>
     );
 }
