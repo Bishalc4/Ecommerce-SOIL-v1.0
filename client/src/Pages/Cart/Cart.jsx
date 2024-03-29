2e39cf2693800dd7789f859dfd8be57972cdeed7
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-03-29 15:00:44 +1100

    Added styling to cart page

--- client/src/Pages/Cart/Cart.jsx
+++ client/src/Pages/Cart/Cart.jsx
@@ -3,9 +3,9 @@ import "./Cart.css"
 //display a sidebar on the side of the page and on click go to a 'CheckOut' page
 function Cart() {
     return(
-        <>
+        <div className="cart-container">
             <h2>Cart Page</h2>
-        </>
+        </div>
     );
 }
 
