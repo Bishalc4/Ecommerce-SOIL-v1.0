bd74c3c69db975e66211e4602e989865b66a8cea
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-03-29 18:54:05 +1100

    modified css files

--- client/src/Components/Layout/Footer/Footer.jsx
+++ client/src/Components/Layout/Footer/Footer.jsx
@@ -1,104 +1,78 @@
 import { Link } from 'react-router-dom';
-import LogoFooter from "../../../assets/SOIL-logo.svg";
+
 import Facebook from "../../../assets/Facebook.svg";
 import Linkedin from "../../../assets/Linkedin.svg";
 import Twitter from "../../../assets/Twitter.svg";
 import Youtube from "../../../assets/Youtube.svg";
-import "./Footer.css";
+import "./Footer.scss";
 
 function Footer() {
   return (
-    <footer>
-      <div className="footer">
-        <div className="footer-padding">
-          <div className="organic-product-column">
-              <h4>Organic Product</h4>
-              <Link to="/">
-                <p>Employee database</p>
-              </Link>
-              <Link to="/">
-                <p>Payroll</p>
-              </Link>
-              <Link to="/">
-                <p>Absences</p>
-              </Link>
-              <Link to="/">
-                <p>Time tracking</p>
-              </Link>
+    <footer className='footer-container'>
+        <div className="help-column" id="footer-column">
+            <h4>Help</h4>
+            <Link to="/">
+              <p>FAQ</p>
+            </Link>
+            <Link to="/">
+              <p>Blog</p>
+            </Link>
+            <Link to="/">
+              <p>Support</p>
+            </Link>
+        </div>
+
+        <div className="about-links-container" id="footer-column">
+            <h4>About SOIL</h4>
+            <Link to="/">
+              <p>About us</p>
+            </Link>
+            <Link to="/">
+              <p>Careers</p>
+            </Link>
+            <Link to="/">
+              <p>Contact us</p>
+            </Link>
+        </div>
+
+        <div className="helpful-link-column" id="footer-column">
+            <h4>Helpful Links</h4>
+            <Link to="/about">
+              <p>About us</p>
+            </Link>
+            <Link to="/">
+              <p>Careers</p>
+            </Link>
+            <Link to="/">
+              <p>Contact us</p>
+            </Link>
+        </div>
+
+        <div className="subscribe-container">
+          <h4>Subscribe</h4>
+          <div>
+            <input type="email" placeholder="Email address" name="subscription-email" required />
           </div>
-
-          <div className="help-column">
-              <h4>Help</h4>
-              <Link to="/">
-                <p>FAQ</p>
-              </Link>
-              <Link to="/">
-                <p>Blog</p>
-              </Link>
-              <Link to="/">
-                <p>Support</p>
-              </Link>
-          </div>
-
-          <div className="diet-nutrition-column">
-              <h4>About SOIL</h4>
-              <Link to="/">
-                <p>About us</p>
-              </Link>
-              <Link to="/">
-                <p>Careers</p>
-              </Link>
-              <Link to="/">
-                <p>Contact us</p>
-              </Link>
-          </div>
-
-          <div className="helpful-link-column">
-              <h4>Helpful Links</h4>
-              <Link to="/">
-                <p>About us</p>
-              </Link>
-              <Link to="/">
-                <p>Careers</p>
-              </Link>
-              <Link to="/">
-                <p>Contact us</p>
-              </Link>
-          </div>
-
-          <div className="footer-subscribe">
-
-            <h4>Subscribe</h4>
-            <div className="subscribe-box">
-                <input type="search" placeholder="Email address" />
-            </div>
-            <p>
-              Hello, we are SOIL. Our goal is to foster Link sustainable food
-              ecosystem that values quality produce and promotes environmental
-              stewardship for all.
-            </p>
-          </div>
-
-          <hr />
-
-          <div className="bottom-footer">
-            <img src={LogoFooter} alt="SOIL_Logo" className="LogoFooter" />
-
-            <div className="policy">
-              <Link to="/"><p>Terms</p></Link>
-              <Link to="/"><p>Privacy</p></Link>
-              <Link to="/"><p>Cookies</p></Link>
-            </div>
-
-            <div className="social-media">
-              <Link to="#"><img src={Facebook} alt="Facebook_Logo" className="Facebook" /></Link>
-              <Link to="#"><img src={Twitter} alt="X_Logo" className="Twitter" /></Link>
-              <Link to="#"><img src={Linkedin} alt="Linkedin_Logo" className="Linkedin" /></Link>
-              <Link to="#"><img src={Youtube} alt="Linkedin_Logo" className="Linkedin" /></Link>
-            </div>
+          <p>
+            Hello, we are SOIL. Our goal is to foster a sustainable food
+            ecosystem that values quality produce and promotes environmental
+            stewardship for all.
+          </p>
+          <div className="social-media-container">
+            <Link to="#">
+              <img src={Facebook} alt="Facebook_Logo" id="social-media-logo" />
+            </Link>
+            <Link to="#">
+              <img src={Twitter} alt="X_Logo" id="social-media-logo" />
+            </Link>
+            <Link to="#">
+              <img src={Linkedin} alt="Linkedin_Logo" id="social-media-logo" />
+            </Link>
+            <Link to="#">
+              <img src={Youtube} alt="Linkedin_Logo" id="social-media-logo" />
+            </Link>
           </div>
         </div>
-      </div>
     </footer>
   );
 }
