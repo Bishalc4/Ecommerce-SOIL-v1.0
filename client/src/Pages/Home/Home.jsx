9f7829a8baee6dd5a74012dbdeccb6f314b49874
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-04-03 20:50:00 +1100

    Added homepage front end design

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,20 +1,56 @@
+import Infographic from "../../assets/Infographic.png"
+import DietNutritionImage from "../../assets/DietNutritionImage.jpg"
 import "./Home.scss"
 
 //This component is for the home/landing page
 function Home() {
     return(
         <div className="home-container">
-            <div className="row">
+            <div className="row" id="landing-section">
                 <div className="overlay">
                     <div className="temp">
                         <h1>SOIL</h1>
                         <p>
-                            At Soil, we’re passionate about offering
-                            premium, organic, and fresh food. At Soil,
+                            At SOIL, we’re passionate about offering
+                            premium, organic, and fresh food. At SOIL,
                             we’re passionate about offering premium,
                             organic, and fresh food.
                         </p>
                         <button className="read-more-btn">Read More</button>
+                    </div>
+                </div>
+            </div>
+            <div className="infographic-container">
+                <div className="infographic">
+                    <img src={Infographic} alt="Organic Foods Infographic"></img>
+                </div>
+            </div>
+            <div className="diet-nutrition-container">
+                <div className="diet-nutrition-image-container">
+                    <img src={DietNutritionImage} alt="Diet Nutrition Image" className="diet-nutrition-img"></img>
+                </div>
+                <div className="diet-nutrition-info">
+                    <h1>Diet & Nutrition</h1>
+                    <p>
+                        Balance Macronutrients: Offer guidance on balancing
+                        macronutrients(carbohydrates, proteins, and fats) in
+                        meals to support overall health and energy levels.
+                        Emphasize the importance of incorporating a variety
+                        of nutrient-dense foods into their diet.
+                    </p>
+                    <button>Read More</button>
+                </div>
+            </div>
+            <div className="community-row">
+                <div className="community-container">
+                    <div className="community-info-container">
+                        <h1>Community</h1>
+                        <p>
+                            At Soil, we’re passionate about offering premium, organic,
+                            and fresh food. At Soil, we’re passionate about offering
+                            premium, organic, and fresh food.
+                        </p>
+                        <button>Read More</button>
                     </div>
                 </div>
             </div>
