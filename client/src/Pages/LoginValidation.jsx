7ff325999279de792f81ecb597af11958bf8742e
Bishalc4 <s4009432@student.rmit.edu.au>
2024-04-03 22:51:33 +1100

    added-comments-to- validation

--- client/src/Pages/LoginValidation.jsx
+++ client/src/Pages/LoginValidation.jsx
@@ -2,26 +2,26 @@ function validate(values) {
     let errors = {};
     
     if (!values.email) {
-        errors.email = 'Email address is required';
-      } else if (!/\S+@\S+\.\S+/.test(values.email)) {
-        errors.email = 'Email address is invalid';
+        errors.email = 'Email address is required';    // check if email field is empty
+      } else if (!/\S+@\S+\.\S+/.test(values.email)) {   
+        errors.email = 'Email address is invalid';   // chech if email is in incorrect format
     }
 
     if(!values.password) {
-        errors.password = 'Password is required';
+        errors.password = 'Password is required';    // check if password field is empty
     }
     if(!/\d/.test(values.password)) {
-        errors.password = 'Include Numbers (0-9)';
+        errors.password = 'Include Numbers (0-9)';  //check if password doesn't include numbers
     }
     if (!/[!@#$%^&*]/.test(values.password)) {
-        errors.password = 'Include Special Characters (@, #, $, etc.)';
+        errors.password = 'Include Special Characters (!,@,#,$,%,^,&,*)';  //check if password doesn't include special character
     }
     if (!/[A-Z]/.test(values.password)) {
-        errors.password = 'Include Uppercase Letters';
+        errors.password = 'Include Uppercase Letters';    // check if password doesn't include Uppercase letters
     }
 
     if (!/[a-z]/.test(values.password)) {
-        errors.password = 'Include Uppercase Letters';
+        errors.password = 'Include Lowercase Letters';   //check if password doesn't include Lowercase letters
     }
 
     return errors;
--- client/src/Pages/SignUpValidation.jsx
+++ client/src/Pages/SignUpValidation.jsx
@@ -1,37 +1,37 @@
 function validate(values) {
     let errors = {};
     if(!values.name) {
-        errors.name = 'Name is required';
+        errors.name = 'Name is required';  //check if name field is empty
     }
 
-    if (!values.email) {
+    if (!values.email) {                 //check if email field is empty
         errors.email = 'Email address is required';
     } else if (!/\S+@\S+\.\S+/.test(values.email)) {
-        errors.email = 'Email address is invalid';
+        errors.email = 'Email address is invalid';    //check if email is in incorrect format
     }
 
     if(!values.password) {
-        errors.password = 'Password is required';
+        errors.password = 'Password is required';   //check if password field is empty
     }
     if(!/\d/.test(values.password)) {
-        errors.password = 'Include Numbers (0-9)';
+        errors.password = 'Include Numbers (0-9)';  //check if password doesn't include numbers
     }
     if (!/[!@#$%^&*]/.test(values.password)) {
-        errors.password = 'Include Special Characters (!,@,#,$,%,^,&,*)';
+        errors.password = 'Include Special Characters (!,@,#,$,%,^,&,*)';  //check if password doesn't include special character
     }
     if (!/[A-Z]/.test(values.password)) {
-        errors.password = 'Include Uppercase Letter';
+        errors.password = 'Include Uppercase Letter';    //check if password doesn't include Uppercase letters
     }
 
     if (!/[a-z]/.test(values.password)) {
-        errors.password = 'Include Uppercase Letters';
+        errors.password = 'Include Lowercase Letters';  // check if password doesn't include Lowercase letters
     }
 
     if(!values.confirmPassword) {
-        errors.confirmPassword = 'Password is not confirmed';
+        errors.confirmPassword = 'Password is not confirmed';   //check if 
     }
     if(values.password != values.confirmPassword) {
-        errors.confirmPassword = 'Password does not match';
+        errors.confirmPassword = 'Password does not match';  //check if password and confirmed password matches 
     }
 
     return errors;
