085d769f495394e978f6e9dafa808e0a43e1ff32
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-04-04 13:29:15 +1100

    username displayed in profile button (header) functionality

--- client/src/Pages/ProfileButton/ProfileButton.jsx
+++ client/src/Pages/ProfileButton/ProfileButton.jsx
@@ -4,7 +4,8 @@ import { IoPersonSharp } from "react-icons/io5";
 
 
 function ProfileButton(props) {
-    const loggedIn = props.loggedIn; 
+    const loggedIn = props.loggedIn;
+    const username = props.username; 
 
     return(
         <div className="profile-container">
@@ -13,17 +14,24 @@ function ProfileButton(props) {
             </button>
             <div className="profile-button-text">
                 {loggedIn ? (
-                    <p className="profile-text">Welcome</p>
+                    <>
+                        <p className="profile-text">Welcome</p>
+                        <p className="profile-my-profile"><b>{username}</b></p>
+                    </>
                 ): (
-                    <p className="profile-text">Log In or Sign Up</p>
+                    <>
+                        <p className="profile-text">Log In or Sign Up</p>
+                        <p className="profile-my-profile"><b>My Profile</b></p>
+                    </>
                 )}
-                <p className="profile-my-profile"><b>My Profile</b></p>
+                
             </div>
         </div>
     );
 }
 ProfileButton.propTypes = {
     loggedIn: PropTypes.bool,
+    username: PropTypes.string,
 }
 
 export default ProfileButton;
