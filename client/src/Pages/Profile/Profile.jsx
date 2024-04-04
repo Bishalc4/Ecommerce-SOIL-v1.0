d64c968a0b7bd6ce55f51b2f2eae64a0d0df908b
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-04-04 13:28:57 +1100

    implemented profile page current user object functionality

--- client/src/Pages/Profile/Profile.jsx
+++ client/src/Pages/Profile/Profile.jsx
@@ -3,23 +3,45 @@ import ProfileAvatar from "../../assets/ProfileAvatar.png"
 import "./Profile.scss"
 
 function Profile() {
+    const currUser = JSON.parse((localStorage.getItem("user")));
+    const users = localStorage.getItem("users");
+    const usersArray = JSON.parse(users);
+    let foundUser = null;
+    for (const user of usersArray) {
+        if (user.name === currUser) {
+            foundUser = user;
+            break;
+        }
+    }
 
+    const dateJoined = new Date(foundUser.dateJoined);
+    const joinDate = dateJoined.toISOString().split('T')[0];
     
+    //signs user out by removing the user key
+    const handleSignOut = () => {
+        if (localStorage.getItem("user")) {
+            localStorage.removeItem("user");
+            console.log("user key has been removed - sign out successful")
+        } else {
+            console.log("user key does not exist - sign out unsuccessful")
+        }
+    }
 
     return(
         <div className="profile-page-container">
-            <h1 className="profile-user">trehanpannya70&apos;s profile</h1>
+            <h1 className="profile-user">{foundUser.name}&apos;s profile</h1>
             <div className="profile-information-container">
                 <img src={ProfileAvatar} alt="Profile Avatar" className="profile-avatar"></img>
                 <div className="profile-details-container">
-                    <p className="profile-username"><b>trehanpannya70</b></p>
-                    <p className="username-age">22 years old</p>
-                    <p className="username-join-date">Member since December 29, 2018</p>
+                    <p className="profile-username"><b>{foundUser.name}</b></p>
+                    <p className="username-join-date">Member since {joinDate}</p>
                     <div className="profile-page-btns">
                         <Link to="/">
                             <button className="edit-profile-btn">Edit Profile</button>
                         </Link>
-                        <button className="edit-profile-btn">Sign Out</button>
+                        <Link to="/">
+                            <button className="edit-profile-btn" onClick={handleSignOut}>Sign Out</button>
+                        </Link>
                     </div>
                 </div>
             </div>
