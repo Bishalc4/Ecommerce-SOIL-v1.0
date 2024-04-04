1f69ee8cd502f1d05ba6c4d77c84dc350485daa8
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-04-04 23:06:36 +1100

    Profile management functional

--- client/src/Pages/EditProfile/EditProfile.jsx
+++ client/src/Pages/EditProfile/EditProfile.jsx
@@ -1,41 +1,100 @@
 import { Link } from 'react-router-dom'
-import React, { useState } from "react";
+import { useState } from "react";
 import "./EditProfile.scss"
 
 function EditProfile() {
     const currUser = JSON.parse((localStorage.getItem("user")));
+
     const users = localStorage.getItem("users");
     var usersArray = JSON.parse(users);
-    let foundUser = null; //found user is the array containing all user information
-    let userIndex = 0;
+
+    const profiles = localStorage.getItem("profiles");
+    var profilesArray = JSON.parse(profiles);
+    
+    let currUserAccount = null; //found user is the array containing all user information
+    let userAccountIndex = 0;
     for (const user of usersArray) {
         if (user.username === currUser) {
-            foundUser = user;
+            currUserAccount = user;
             break;
         }
-        userIndex++;
+        userAccountIndex++;
+    }
+
+    let currUserProfile = null;
+    let userProfileIndex = 0;
+    for (const user of profilesArray) {
+        if (user.username === currUser) {
+            currUserProfile = user;
+            break;
+        }
+        userProfileIndex++;
     }
     
-    const [userDetails, setUserDetails] = useState({username: foundUser.username,
-                                                    email: foundUser.email,
-                                                    firstName: foundUser.firstName,
-                                                    lastName: foundUser.lastName
+    const [userDetails, setUserDetails] = useState({username: currUserAccount.username,
+                                                    email: currUserAccount.email,
+                                                    firstName: currUserAccount.firstName,
+                                                    lastName: currUserAccount.lastName
                                                     });
+
+    const [profileDetails, setProfileDetails] = useState({age: currUserProfile.age,
+                                                          weight: currUserProfile.weight,
+                                                          height: currUserProfile.height,
+                                                          activityLevel: currUserProfile.activityLevel,
+                                                          dietaryPreferences: currUserProfile.dietaryPreferences,
+                                                          healthGoals: currUserProfile.healthGoals
+                                                        });
     
     function handleFirstNameChange(e) {
         setUserDetails({...userDetails, firstName: e.target.value.replace(/\s/g, '')});
+        console.log(userDetails);
     }
 
     function handleLastNameChange(e) {
         setUserDetails({...userDetails, lastName: e.target.value.replace(/\s/g, '')});
     }
 
-    function handleSaveChanges(e) {
+    function handleAccountSaveChanges(e) {
         e.preventDefault();
-        usersArray[userIndex].email = userDetails.email;
-        usersArray[userIndex].firstName = userDetails.firstName;
-        usersArray[userIndex].lastName = userDetails.lastName;
+        usersArray[userAccountIndex].email = userDetails.email;
+        usersArray[userAccountIndex].firstName = userDetails.firstName;
+        usersArray[userAccountIndex].lastName = userDetails.lastName;
         localStorage.setItem("users", JSON.stringify(usersArray));
+    }
+
+    function handleAgeChange(e) {
+        setProfileDetails({...profileDetails, age: e.target.value});
+    }
+
+    function handleHeightChange(e) {
+        setProfileDetails({...profileDetails, height: e.target.value});
+    }
+
+    function handleWeightChange(e) {
+        setProfileDetails({...profileDetails, weight: e.target.value});
+    }
+
+    function handleActivityLevelChange(e) {
+        setProfileDetails({...profileDetails, activityLevel: e.target.value});
+    }
+
+    function handleDietaryPreferenceChange(e) {
+        setProfileDetails({...profileDetails, dietaryPreferences: e.target.value});
+    }
+
+    function handleHealthGoalChange(e) {
+        setProfileDetails({...profileDetails, healthGoals: e.target.value});
+    }
+
+    function handleProfileSaveChanges(e) {
+        e.preventDefault();
+        profilesArray[userProfileIndex].age = profileDetails.age;
+        profilesArray[userProfileIndex].height = profileDetails.height;
+        profilesArray[userProfileIndex].weight = profileDetails.weight;
+        profilesArray[userProfileIndex].activityLevel = profileDetails.activityLevel;
+        profilesArray[userProfileIndex].dietaryPreferences = profileDetails.dietaryPreferences;
+        profilesArray[userProfileIndex].healthGoals = profileDetails.healthGoals;
+        localStorage.setItem("profiles", JSON.stringify(profilesArray));
     }
 
     return(
@@ -68,20 +127,28 @@ function EditProfile() {
                         <button>Change password</button>
                     </Link>
                     <div className="form-submit-container">
-                        <button onClick={handleSaveChanges}>Save Changes</button>
+                        <button onClick={handleAccountSaveChanges}>Save Changes</button>
                     </div>
                 </form>
             </div>
             <div className="goals-container">
                 <h2>Goals:</h2>
                 <form className="goals-form">
+                    <div className="age-group">
+                        <label>Age:</label>
+                        <input type="number" min="1" step="1" onChange={handleAgeChange}></input>
+                    </div>
+                    <div className="height-group">
+                        <label>Height:</label>
+                        <input type="number" min="0.01" step="any" onChange={handleHeightChange}></input>
+                    </div>
                     <div className="weight-group">
                         <label>Weight:</label>
-                        <input type="number"></input>
+                        <input type="number" min="0.01" step="any" onChange={handleWeightChange}></input>
                     </div>
                     <div className="activity-level-group">
                         <label htmlFor="activity-levels">Activity level:</label>
-                        <select id="activity-levels" name="activity-level">
+                        <select id="activity-levels" name="activity-level" onChange={handleActivityLevelChange}>
                             <option value="sedentary">Sedentary: little to no excercise</option>
                             <option value="light">Light: excercise 1-3 times/week</option>
                             <option value="moderate">Moderate: excercise 4-5 times/week</option>
@@ -92,9 +159,9 @@ function EditProfile() {
                     </div>
                     <div className="dietary-preferences-group">
                         <label htmlFor="dietary-preferences">Dietary preferences:</label>
-                        <select id="dietary-preferences" name="dietary-preference">
+                        <select id="dietary-preferences" name="dietary-preference" onChange={handleDietaryPreferenceChange}>
                             <option value="vegan">Vegan</option>
-                            <option value="vegetarian">Vegetarian</option>
+                            <option value="vegetarian">Vegetarian -</option>
                             <option value="pescatarian">Pescatarian</option>
                             <option value="paleo">Paleo</option>
                             <option value="keto">Keto</option>
@@ -104,7 +171,7 @@ function EditProfile() {
                     </div>
                     <div className="health-goal-group">
                         <label htmlFor="health-goals">Health goal:</label>
-                        <select id="health-goals" name="health-goal">
+                        <select id="health-goals" name="health-goal" onChange={handleHealthGoalChange}>
                             <option value="maintain">Maintain weight</option>
                             <option value="mild-loss">Mild weight loss</option>
                             <option value="loss">Weight loss</option>
@@ -115,7 +182,7 @@ function EditProfile() {
                         </select>
                     </div>
                     <div className="form-submit-container">
-                        <button>Save Changes</button>
+                        <button onClick={handleProfileSaveChanges}>Save Changes</button>
                     </div>
                 </form>
             </div>
