a5844c07279d64fea2cead81d567694628700dc6
Bishalc4 <s4009432@student.rmit.edu.au>
2024-04-04 23:53:59 +1100

    products List- implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import { useLocation } from "react-router-dom"
 import Header from './Components/Layout/Header/Header.jsx'
 import Navbar from './Components/Layout/Navbar/Navbar.jsx'
 import Footer from './Components/Layout/Footer/Footer.jsx'
+import ProductsList from "../src/Pages/ProductList.jsx"
 import AllRoutes from "./Pages/AllRoutes.jsx"
 
 import './App.css'
 
 function App() {
+  ProductsList();
   
   const location = useLocation();
   const isLoginPage = (location.pathname === '/login' || location.pathname === '/signup');
