492ae20c06183ae4af77ebab7ca8b87ae465f0e0
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-04-06 02:31:54 +1100

    import all images through an index.js file

---
+++ client/src/assets/index.js
@@ -0,0 +1,45 @@
+import apple from "./ProductsImage/apple.jpg";
+import banana from "./ProductsImage/banana.jpg";
+import cucumber from "./ProductsImage/cucumber.jpg";
+import tomato from "./ProductsImage/tomato.jpg";
+import potato from "./ProductsImage/potato.jpg";
+import carrot from "./ProductsImage/carrot.jpg";
+import orange from "./ProductsImage/orange.jpg";
+import broccoli from "./ProductsImage/broccoli.jpg";
+import spinach from "./ProductsImage/spinach.jpg";
+import grapes from "./ProductsImage/grapes.jpg";
+import lettuce from "./ProductsImage/lettuce.jpg";
+import strawberry from "./ProductsImage/strawberry.jpg";
+import bell_pepper from "./ProductsImage/bell_pepper.jpg";
+import watermelon from "./ProductsImage/watermelon.jpg";
+import onion from "./ProductsImage/onion.jpg";
+import pineapple from "./ProductsImage/pineapple.jpg";
+import zucchini from "./ProductsImage/zucchini.jpg";
+import kiwi from "./ProductsImage/kiwi.jpg";
+import mango from "./ProductsImage/mango.jpg";
+import lemon from "./ProductsImage/lemon.jpg";
+
+const images = {
+    apple,
+    banana,
+    cucumber,
+    tomato,
+    potato,
+    carrot,
+    orange,
+    broccoli,
+    spinach,
+    grapes,
+    lettuce,
+    strawberry,
+    bell_pepper,
+    watermelon,
+    onion,
+    pineapple,
+    zucchini,
+    kiwi,
+    mango,
+    lemon
+};
+
+export default images;
