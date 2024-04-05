7e22ee3a4e58d42730d565274c3232c50c042bb9
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-04-06 01:55:00 +1100

    ProductCard now received props from local storage

--- client/src/Components/Layout/ProductCard/ProductCard.jsx
+++ client/src/Components/Layout/ProductCard/ProductCard.jsx
@@ -3,26 +3,34 @@ import "./ProductCard.scss"
 import { Link } from "react-router-dom"
 
 function ProductCard(props) {
-    const productImage = props.productImage;
-    const productPrice = parseFloat(props.productPrice).toFixed(2);
-    const productName = props.productName;
+    const productId = props.product.productId;
+    const productImage = props.product.imageUrl; //this will be passed through the Specials page
+    const productPrice = parseFloat(props.product.price).toFixed(2);
+    const productSpecialPrice = parseFloat(props.product.specialPrice).toFixed(2);
+    const productName = props.product.productName;
+    
 
     return(
         <div className="product-card-container">
-            <Link to="/product" state={{ productId: props.productId}}>
+            <Link to="/product" state={{ productId: {productId}}}>
                 <img src={productImage} alt="Product Image"/>
             </Link>
-            <h1>${productPrice}</h1>
+            {productSpecialPrice !== null ? (
+                <>
+                    <h1>${productSpecialPrice}</h1>
+                    <h2 className="product-original-price">${productPrice}</h2>
+                </>
+            ): (
+                <h1>${productPrice}</h1>
+            )}
+            
             <p>{productName}</p>
             <button>Add to cart</button>
         </div>
     );
 }
 ProductCard.propTypes = {
-    productImage: PropTypes.string.isRequired,
-    productPrice: PropTypes.number.isRequired,
-    productName: PropTypes.string.isRequired,
-    productId: PropTypes.number.isRequired,
+    product: PropTypes.object,
 }
 
 export default ProductCard
