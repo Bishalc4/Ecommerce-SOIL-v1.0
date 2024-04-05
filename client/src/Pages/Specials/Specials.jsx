f9c855008c32e7301e6e8bece30dd53b2b5dda99
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-04-06 01:54:41 +1100

    Specials renders on special products

--- client/src/Pages/Specials/Specials.jsx
+++ client/src/Pages/Specials/Specials.jsx
@@ -1,22 +1,28 @@
-import "./Specials.scss";
+import { useState, useEffect } from 'react';
 import ProductCard from "../../Components/Layout/ProductCard/ProductCard";
-import banana from "../../assets/ProductsImage/banana.jpg";
+// import banana from "../../assets/ProductsImage/banana.jpg";
+import "./Specials.scss";
 
 //Special deals page - displays products on sale
 function Specials(){
+
+    const [product, setProduct] = useState([]);
+
+    useEffect(() => {
+        const storedProduct = JSON.parse(localStorage.getItem("products"));
+        if (storedProduct) {
+            setProduct(storedProduct);
+        }
+    }, []);
+
     return(
         <div className="specials-container">
             <div className="products-grid-container">
-                <ProductCard productId={1} productName="Banana" productPrice={0.7} productImage={banana}/>
-                <ProductCard productName="Banana" productPrice={0.7} productImage={banana}/>
-                <ProductCard productName="Banana" productPrice={0.7} productImage={banana}/>
-                <ProductCard productName="Banana" productPrice={0.7} productImage={banana}/>
-                <ProductCard productName="Banana" productPrice={0.7} productImage={banana}/>
-                <ProductCard productName="Banana" productPrice={0.7} productImage={banana}/>
-                <ProductCard productName="Banana" productPrice={0.7} productImage={banana}/>
-                <ProductCard productName="Banana" productPrice={0.7} productImage={banana}/>
-                <ProductCard productName="Banana" productPrice={0.7} productImage={banana}/>
-                <ProductCard productName="Banana" productPrice={0.7} productImage={banana}/>
+                {product
+                    .filter(item => item.specialPrice !== null)
+                    .map((item, index) => (
+                    <ProductCard key={index} product={item} />
+                ))}
             </div>
         </div>
     );
