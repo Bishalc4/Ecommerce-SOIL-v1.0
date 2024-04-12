18462be9d760b878990031edf7ba321257f27e87
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-04-12 19:10:40 +1000

    Implemented add to meal button

---
+++ client/src/Components/Layout/AddMealBtn/AddMealBtn.jsx
@@ -0,0 +1,24 @@
+
+function AddMealBtn() {
+
+    //should add to a local storage button
+    /*local storage should contain the following information:
+        Sunday = [Breafast, Lunch, Dinner] -> max limit is 5 meals
+        Monday = [Breafast, Lunch, Dinner] -> max limit is 5 meals
+        Tuesday = [Breafast, Lunch, Dinner] -> max limit is 5 meals
+        Wednesday = [Breafast, Lunch, Dinner] -> max limit is 5 meals
+        Thursday = [Breafast, Lunch, Dinner] -> max limit is 5 meals
+        Friday = [Breafast, Lunch, Dinner] -> max limit is 5 meals
+        Saturday = [Breafast, Lunch, Dinner] -> max limit is 5 meals */
+
+    /*When a user clicks on the add to planner button it should show up
+    with a pop up asking them to select a day and a meal (cannot add to the same meal)*/
+
+    return(
+        <>
+            <button>Add to planner</button>
+        </>
+    );
+}
+
+export default AddMealBtn
