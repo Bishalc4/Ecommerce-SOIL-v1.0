112f2572ec6bdf6e59552ac3f2d263688fdbf14e
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-04-16 14:07:16 +1000

    Meal plan component created

---
+++ client/src/Components/Layout/MealPlan/MealPlan.jsx
@@ -0,0 +1,21 @@
+import DailyMealPlan from "../DailyMealPlan/DailyMealPlan";
+import "./MealPlan.scss"
+
+function MealPlan() {
+    return(
+        <div className="meal-container">
+            <h1>Your Weekly Meal Plan</h1>
+            <div className="daily-meals-container">
+                <DailyMealPlan className="day-meal" day="Sunday" />
+                <DailyMealPlan className="day-meal" day="Monday" />
+                <DailyMealPlan className="day-meal" day="Tuesday" />
+                <DailyMealPlan className="day-meal" day="Wednesday" />
+                <DailyMealPlan className="day-meal" day="Thursday" />
+                <DailyMealPlan className="day-meal" day="Friday" />
+                <DailyMealPlan className="day-meal" day="Saturday" />
+            </div>
+        </div>
+    );
+}
+
+export default MealPlan
