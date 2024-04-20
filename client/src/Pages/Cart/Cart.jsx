71c1a2e1dae8d3d4dd65d4da043ef4c06a509000
Bishalc4 <s4009432@student.rmit.edu.au>
2024-04-21 00:59:38 +1000

    fixed-cart page

--- client/src/Pages/Cart/Cart.jsx
+++ client/src/Pages/Cart/Cart.jsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import images from "../../assets";
+import emptyCart from "../../assets/EmptyCart.jpg"
 import "./Cart.scss";
 
 function Cart() {
@@ -9,21 +10,23 @@ function Cart() {
     const [price, setPrice] = useState(0);
     const [cartData, setCartData] = useState([]);
 
-    const productsList = JSON.parse(localStorage.getItem("products"));  // get the list of product from localstorage
+    const productsList = JSON.parse(localStorage.getItem("products"));  // get the list of products from localStorage
     const user = JSON.parse(localStorage.getItem("user"));   // get the logged in user
 
+    // Retrieve cart data from local storage once
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+    const userCart = cart.find(item => item.username === user);
     useEffect(() => {
-        const cart = JSON.parse(localStorage.getItem("cart"));
-        const userCart = cart.find(item => item.username === user);
         if (userCart) {                                    // if the user has a cart, get the cart data (productId and Quantity)
             setCartData(userCart.data);
         }
-    }, []);
+    }, [userCart]);
 
-    function getProductById (productId) {
+    function getProductById(productId) {
         return productsList.find(product => product.productId === productId);    // get the product detail from productlist using the productId stored in cart
     }
-     function handleTotalPrice() {
+
+    function handleTotalPrice() {                                              // handle the total amount of the cart
         let total = 0;
         cartData.forEach(item => {
             const product = getProductById(item.productId);
@@ -38,23 +41,20 @@ function Cart() {
         handleTotalPrice();
     }, [cartData]);
 
-    function handleDelete(itemId) {
+    function handleDelete(itemId) {      
         const updatedCartData = cartData.filter(item => item.productId !== itemId);
         setCartData(updatedCartData);
-        const updatedCart = JSON.parse(localStorage.getItem("cart") || "[]");
-    
+        const updatedCart = [...cart]; // Copy the cart array
         const userCartIndex = updatedCart.findIndex(cartItem => cartItem.username === user);
         if (userCartIndex >= 0) {
             updatedCart[userCartIndex].data = updatedCartData;
             localStorage.setItem("cart", JSON.stringify(updatedCart));
         }
     }
-    
-    
 
     function handleQuantityChange(productId, newQuantity) {
-        if (newQuantity > 0) {                                          // Update local storage
-            const updatedCart = JSON.parse(localStorage.getItem("cart")) || [];
+        if (newQuantity > 0) {
+            const updatedCart = [...cart]; // Copy the cart array
             const userCartIndex = updatedCart.findIndex(cartItem => cartItem.username === user);
             if (userCartIndex !== -1) {
                 const productToUpdate = updatedCart[userCartIndex].data.find(item => item.productId === productId);
@@ -66,50 +66,60 @@ function Cart() {
             const updatedCartData = cartData.map(item =>
                 item.productId === productId ? { ...item, quantity: newQuantity } : item
             );
-            setCartData(updatedCartData);                                   // Update state
+            setCartData(updatedCartData);
         }
     }
 
     function handleSubmit() {
         navigate("/checkout", { state: { price, cartData } }); 
     }
-    
 
     return (
         <>
-        <div className='heading-title'>
-            <h3>Product</h3>
-            <h3>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Quantity</h3>
-            <h3>Price &nbsp;&nbsp;Remove</h3>
-        </div>
-        <div className='cartPage-container'> {
-                cartData.map((item, index) => (
+        {cartData.length > 0 && (
+            <div className='heading-title'>
+                <h3>Product</h3>
+                <h3>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Quantity</h3>
+                <h3>Price &nbsp;&nbsp;Remove</h3>
+            </div>
+        )}
+        {cartData.length > 0 && (
+            <div className='cartPage-container'>
+                {cartData.map((item, index) => (
                     <div key={index} className="cart-container">
-                    <div className='cart-detail'>
-                        <img src={images[getProductById(item.productId)?.imageUrl]} alt={getProductById(item.productId)?.productName} />
-                        <p>{getProductById(item.productId)?.productName}</p>
-                    </div>
-                    <div className="quantity-buttons">
-                        <button onClick={() => handleQuantityChange(item.productId, item.quantity - 1)}> - </button>
-                        <button>{item.quantity}</button>
-                        <button onClick={() => handleQuantityChange(item.productId, item.quantity + 1)}> + </button>
-                    </div>
-                    <div className="price-and-delete">
-                        <span>${getProductById(item.productId)?.specialPrice !== null ? getProductById(item.productId)?.specialPrice.toFixed(2) 
-                        : getProductById(item.productId)?.price.toFixed(2)}</span>
-                        <button onClick={() => handleDelete(item.productId)}>Delete</button>
-                    </div>
+                        <div className='cart-detail'>
+                            <img src={images[getProductById(item.productId)?.imageUrl]} alt={getProductById(item.productId)?.productName} />
+                            <p>{getProductById(item.productId)?.productName}</p>
+                        </div>
+                        <div className="quantity-buttons">
+                            <button onClick={() => handleQuantityChange(item.productId, item.quantity - 1)}> - </button>
+                            <button>{item.quantity}</button>
+                            <button onClick={() => handleQuantityChange(item.productId, item.quantity + 1)}> + </button>
+                        </div>
+                        <div className="price-and-delete">
+                            <span>${getProductById(item.productId)?.specialPrice !== null ? getProductById(item.productId)?.specialPrice.toFixed(2) 
+                            : getProductById(item.productId)?.price.toFixed(2)}</span>
+                            <button onClick={() => handleDelete(item.productId)}>Delete</button>
+                        </div>
                     </div>
                 ))}
                 <div className='cartTotal'>
-                <span className='total-title'>Total Amount of your Cart</span>
-                <span className='total-amount'>${price.toFixed(2)}</span>
+                    <span className='total-title'>Total Amount of your Cart</span>
+                    <span className='total-amount'>${price.toFixed(2)}</span>
                 </div>
-                 <div className='cartProceed'>
-                    <button onClick={handleSubmit}>Procced</button>
-                 </div>
+                <div className='cartProceed'>
+                    <button onClick={handleSubmit}>Proceed</button>
+                </div>
             </div>
-            </>
+        )}
+        {cartData.length === 0 && (
+            <div className='empty-cart'>
+                <p>Your shopping cart is empty.</p>
+                <img src={emptyCart} alt="Empty Cart" />
+                <button onClick={() => navigate("/specials")}>Continue shopping</button>
+            </div>
+        )}
+        </>
     );
 }
 
