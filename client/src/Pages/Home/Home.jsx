ff4f8eb6d21760522a2daf34d2bef7f4af11da25
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-04-21 12:11:04 +1000

    Home page infographic

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,4 +1,10 @@
 // import Infographic from "../../assets/Infographic.png"
+import pesticides from "../../assets/pesticides.png";
+import animalWelfare from "../../assets/animalWelfare.png"
+import GMO from "../../assets/GMO.png"
+import conservation from "../../assets/conservation.png"
+import sustainable from "../../assets/sustainable.png"
+import vitamins from "../../assets/vitamins.png"
 import { Link } from 'react-router-dom';
 import DietNutritionImage from "../../assets/DietNutritionImage.jpg"
 import "./Home.scss"
@@ -22,9 +28,69 @@ function Home() {
                 </div>
             </div>
 
-            {/* <div className="infographic-container">
-                <img src={Infographic} alt="Organic Foods Infographic"></img>
-            </div> */}
+            <div className='infographic-container'>
+                <div className='infographic-row'>
+                    <div className="infographic-section">
+                        <img src={pesticides} className="infographic-image"></img>
+                        <div className="infographic-information-container">
+                            <h3>No pesticides or fertilizers</h3>
+                            <p>Promoting healthier diets and reducing
+                                the risk of exposure to harmful chemicals.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="infographic-section">
+                        <img src={sustainable} className="infographic-image"></img>
+                        <div className="infographic-information-container">
+                            <h3>Sustainable packaging</h3>
+                            <p>All products come in sustainable packaging options,
+                                using biodegradable materials, reducing waste.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="infographic-section">
+                        <img src={vitamins} className="infographic-image"></img>
+                        <div className="infographic-information-container">
+                            <h3>Essential vitamins and minerals</h3>
+                            <p>Organic foods contain higher levels of essential
+                                vitamins, mineralsand antioxidants compared to
+                                conventionally grown foods.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className='infographic-row'>
+                    <div className="infographic-section">
+                        <img src={conservation} className="infographic-image"></img>
+                        <div className="infographic-information-container">
+                            <h3>Conservation</h3>
+                            <p>Organic farming priorotises soil and water
+                                conservation, biodiversity and reducing
+                                polution.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="infographic-section">
+                        <img src={animalWelfare} className="infographic-image"></img>
+                        <div className="infographic-information-container">
+                            <h3>Animal welfare</h3>
+                            <p>Organic dairy farming standards prioritise humane
+                                treatment, providing animals with access to
+                                outdoor spaces and natural diet.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="infographic-section">
+                        <img src={GMO} className="infographic-image"></img>
+                        <div className="infographic-information-container">
+                            <h3>No GMO</h3>
+                            <p>Organic foods are produced without genetically
+                                modified organisms (GMOs).
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             <div className="diet-nutrition-container">
                 <img src={DietNutritionImage} alt="Diet Nutrition Image" className="diet-nutrition-img"></img>
@@ -48,9 +114,9 @@ function Home() {
                     <div className="community-info-container">
                         <h1>Community</h1>
                         <p>
-                            At Soil, we’re passionate about offering premium, organic,
-                            and fresh food. At Soil, we’re passionate about offering
-                            premium, organic, and fresh food.
+                            We’re committed to empowering our community to
+                            cultivate their own fresh, organic vegetables, fostering
+                            self-sustainability and healthy living.
                         </p>
                         <Link to="/growityourself">
                             <button>Read More</button>
