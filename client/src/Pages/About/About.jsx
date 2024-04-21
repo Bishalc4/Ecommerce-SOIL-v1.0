bc903fbaea7982c0226e31fe360623f499eb4cb4
Bishalc4 <s4009432@student.rmit.edu.au>
2024-04-21 21:45:26 +1000

    about page added

--- client/src/Pages/About/About.jsx
+++ client/src/Pages/About/About.jsx
@@ -4,26 +4,33 @@ function About() {
     return(
         <div className="about-container">
             <div>
-            Welcome to SOIL, where we cultivate more than just organic food, we encourage
-             individuals to make healthier choices for themselves and the planet.
+                Welcome to SOIL, where we cultivate more than just organic food, we encourage
+                individuals to make healthier choices for themselves and the planet.
             </div>
             <div>
-            SOIL isn't just your average grocer; we're a long-term organic food grocer with several store locations around Melbourne.
-            Our mission is simple: to provide premium, organic fresh food to our community while giving advice on diet, nutrition, and sustainable farming practices. 
-            We go beyond of just online shopping by offering face-to-face seminars, creating meaningful connections with our customers, 
-            and building a sense of belonging within our community.
+                SOIL isn't just your average grocer; we're a long-term organic food grocer with several store locations around Melbourne.
+                Our mission is simple: to provide premium, organic fresh food to our community while giving advice on diet, nutrition, and sustainable farming practices. 
+                We go beyond of just online shopping by offering face-to-face seminars, creating meaningful connections with our customers, 
+                and building a sense of belonging within our community.
             </div>
-            <h2> we offer </h2>
+            <h2> WE OFFER </h2>
             <div> 
-                Seamless Online Shopping: Say goodbye to the pain of in-store shopping. 
+            <strong>Seamless Online Shopping:</strong> Say goodbye to the pain of in-store shopping. 
                 Our website feature a user-friendly interface that allows customers to browse, purchase, and do home deliveries with ease.
             </div>
+                <br></br>
             <div> 
-            Search Functionality: Looking for a specific product? Our search feature will make it easier than ever to find what you're looking for, saving you time and frustration.
+                <strong>Search Functionality:</strong> Looking for a specific product? Our search feature will make it easier than ever to find what you're looking for, saving you time and frustration.
             </div>
+                <br></br>
             <div> 
-            Meal Planner: Looking for a daily or weekly meal plans based on your pefernces and goals? Our website offers feature where you can do this with ease.
+                <strong>Meal Planner:</strong> Looking for a daily or weekly meal plans based on your pefernces and goals? Our website offers feature where you can do this with ease.
             </div>
+
+            <h2>Contact our Team</h2>
+                <strong>Pannya Trehan: </strong>  s4003909@student.rmit.edu.au
+                <br></br>
+                <strong>Bishal Chhetri: </strong>  s4009432@student.rmit.edu.au
         </div>
     );
 }
